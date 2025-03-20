a64a9ecbd21e992b19c1871e250bd62716c89a8c
chindesu0207 <potter0207@gmail.com>
2025-03-20 15:22:20 +0800

    refactor: 調整 Response type 以符合回傳的資料格式，並調整 getAll api 加入分頁功能

--- src/api/services/product/index.ts
+++ src/api/services/product/index.ts
@@ -3,29 +3,41 @@ import { ProductResponse, ProductSchemaRequestType } from "./types";
 
 export const productApi = {
   baseUrl: "/api" + import.meta.env.VITE_API_PATH + "/admin",
-  getAll: async () => {
+  getAll: async (page: number) => {
     const res: ProductResponse = await axiosClient.get(
-      `${productApi.baseUrl}/products`
+      `${productApi.baseUrl}/products?page=${page}`,
     );
     return res;
   },
   create: async (data: ProductSchemaRequestType) => {
     const res: ProductResponse = await axiosClient.post(
       `${productApi.baseUrl}/product`,
-      data
+      data,
     );
     return res;
   },
   update: async (id: string, data: ProductSchemaRequestType) => {
     const res: ProductResponse = await axiosClient.put(
       `${productApi.baseUrl}/product/${id}`,
-      data
+      data,
     );
     return res;
   },
   delete: async (id: string) => {
     const res: ProductResponse = await axiosClient.delete(
-      `${productApi.baseUrl}/product/${id}`
+      `${productApi.baseUrl}/product/${id}`,
+    );
+    return res;
+  },
+  uploadImage: async (data: FormData) => {
+    const res: ProductResponse = await axiosClient.post(
+      `${productApi.baseUrl}/upload`,
+      data,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      },
     );
     return res;
   },
--- src/api/services/product/types.ts
+++ src/api/services/product/types.ts
@@ -2,9 +2,19 @@ import { productSchema } from "@/schema/productSchema";
 import { Product } from "@/types/product";
 import { z } from "zod";
 
+interface Pagination {
+  category: string;
+  current_page: number;
+  has_next: boolean;
+  has_pre: boolean;
+  total_pages: number;
+}
+
 export interface ProductResponse {
   success: true;
-  products: Product[];
+  pagination?: Pagination;
+  products?: Product[];
+  imageUrl?: string;
 }
 
 export type ProductSchemaType = z.infer<typeof productSchema>;
